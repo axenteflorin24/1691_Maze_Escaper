549b9abfb29553bedc737eaca39712a54cc3cd13
axenteflorin24 <148908673+axenteflorin24@users.noreply.github.com>
2025-02-20 09:23:38 -0800

    Add files via upload

---
+++ levels/level_2/level_2.gd
@@ -0,0 +1,36 @@
+extends Node3D
+
+var time = 224
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+	#Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
+	ambience_sound(delta)
+	
+func ambience_sound(delta):
+	time =  time-delta
+	
+	if time < 1.8:
+		time = 224+1.8
+		$sfx/AudioStreamPlayer2.set("volume_db", 14.68)
+		$sfx/AudioStreamPlayer2.play()
+		$timers/ambience2.start()
+	if $timers/ambience2.is_stopped():
+		if $sfx/AudioStreamPlayer2.get("playing") == true:
+			$sfx/AudioStreamPlayer2.set("volume_db", -80)
+			$sfx/AudioStreamPlayer2.stop()
+	if $timers/ambience.is_stopped():
+		$sfx/AudioStreamPlayer.set("volume_db", -80)
+		$sfx/AudioStreamPlayer.stop()
+		$sfx/AudioStreamPlayer.set("volume_db", 14.68)
+		$sfx/AudioStreamPlayer.play()
+		$timers/ambience.start()
+		
+	else:
+		if $timers/ambience.is_stopped():
+			$sfx/AudioStreamPlayer.play()
